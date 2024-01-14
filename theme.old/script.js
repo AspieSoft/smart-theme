8cf0d732d8081dae2eb1be7709fc1dec195f1ca2
AspieSoft <aspiesoftweb@gmail.com>
2024-01-14 00:35:37 -0500

    updated config layout

---
+++ theme.old/script.js
@@ -0,0 +1,115 @@
+;function sleep(ms){return new Promise(resolve => setTimeout(resolve, ms));}
+
+async function onReady(cb){
+  if(document.readyState !== 'loading'){
+    cb();
+  }else{
+    let ranCB = false;
+    document.addEventListener('DOMContentLoaded', function () {
+      ranCB = true;
+      cb();
+    });
+
+    sleep(100);
+
+    if(!ranCB){
+      let loops = 10;
+      while(loops > 0){
+        if(document.readyState !== 'loading'){
+          cb();
+          return;
+        }
+
+        loops--;
+        sleep(250);
+      }
+    }
+  }
+}
+
+onReady(async function(){
+  const body = document.body || document.querySelector('body');
+
+  function onInterval(){
+    document.querySelectorAll('.widget > *, .sidebar > *').forEach(function(elm) {
+      if(elm.clientHeight < window.innerHeight - 150) {
+        elm.classList.add('widget-smaller-than-vh');
+      }else{
+        elm.classList.remove('widget-smaller-than-vh');
+      }
+    });
+
+    document.querySelectorAll('header .header-image, footer').forEach(function(elm){
+      if(elm.clientHeight < 300){
+        elm.classList.remove('background-size-w', 'background-size-h');
+        return;
+      }
+
+      if(elm.classList.contains('background-size-w') || elm.classList.contains('background-size-h')){
+        return;
+      }
+
+      const css = window.getComputedStyle(elm);
+      if(typeof css['background-size'] !== 'string' || typeof css['background-image'] !== 'string'){
+        return;
+      }
+
+      if(css['background-size'].includes('cover')){
+        css['background-image'].replace(/url\s*\(\s*(["'`])(.*?)\1\s*\)/i, function(_, _, url){
+          const img = new Image();
+          img.src = url;
+          img.onload = function(){
+            if(img.width !== 0 && img.height !== 0){
+              elm.setAttribute('img-width', img.width);
+              elm.setAttribute('img-height', img.height);
+              if(elm.clientWidth / img.width >= elm.clientHeight / img.height){
+                elm.classList.add('background-size-w');
+                elm.classList.remove('background-size-h');
+              }else{
+                elm.classList.add('background-size-h');
+                elm.classList.remove('background-size-w');
+              }
+            }
+            img.remove();
+          }
+        });
+      }
+    });
+  }
+  onInterval();
+  setInterval(onInterval, 1000);
+
+  function onResize(){
+    document.querySelectorAll('header .header-image, footer').forEach(function(elm){
+      if(elm.clientHeight < 300){
+        elm.classList.remove('background-size-w', 'background-size-h');
+        return;
+      }
+
+      if(!elm.classList.contains('background-size-w') && !elm.classList.contains('background-size-h')){
+        return;
+      }
+
+      const imgWidth = elm.getAttribute('img-width');
+      const imgHeight = elm.getAttribute('img-height');
+      if(!imgWidth || !imgHeight || imgWidth === 0 || imgHeight === 0){
+        return;
+      }
+      if(elm.clientWidth / imgWidth >= elm.clientHeight / imgHeight){
+        elm.classList.add('background-size-w');
+        elm.classList.remove('background-size-h');
+      }else{
+        elm.classList.add('background-size-h');
+        elm.classList.remove('background-size-w');
+      }
+    });
+  }
+  onResize();
+  window.addEventListener('resize', onResize, {passive: true});
+
+  /* function onFastInterval(){
+    
+  }
+  onFastInterval();
+  setInterval(onFastInterval, 300); */
+});
