c64cf53e7f5fa8a92dc861f11348a63a4de98043
AspieSoft <aspiesoftweb@gmail.com>
2024-01-17 15:01:38 -0500

    updated

--- theme/script.js
+++ theme/script.js
@@ -41,8 +41,73 @@ onReady(async function(){
         elm.classList.remove('widget-smaller-than-vh');
       }
     });
+
+    document.querySelectorAll('header .header-img, footer').forEach(function(elm){
+      if(elm.clientHeight < 300){
+        elm.classList.remove('background-size-w', 'background-size-h');
+        return;
+      }
+
+      if(elm.classList.contains('background-size-w') || elm.classList.contains('background-size-h')){
+        return;
+      }
+
+      const css = window.getComputedStyle(elm);
+      if(typeof css['background-size'] !== 'string' || typeof css.getPropertyValue('--color-img') !== 'string'){
+        return;
+      }
+
+      if(css['background-size'].includes('cover')){
+        css.getPropertyValue('--color-img').replace(/url\s*\(\s*(["'`])(.*?)\1\s*\)/i, function(_, _, url){
+          const img = new Image();
+          img.src = url;
+          img.onload = function(){
+            if(img.width !== 0 && img.height !== 0){
+              elm.setAttribute('img-width', img.width);
+              elm.setAttribute('img-height', img.height);
+              if(elm.clientWidth / img.width >= elm.clientHeight / img.height){
+                elm.classList.add('background-size-w');
+                elm.classList.remove('background-size-h');
+              }else{
+                elm.classList.add('background-size-h');
+                elm.classList.remove('background-size-w');
+              }
+            }
+            img.remove();
+          }
+        });
+      }
+    });
   }
   onInterval();
   setInterval(onInterval, 1000);
+
+  function onResize(){
+    document.querySelectorAll('header .header-img, footer').forEach(function(elm){
+      if(elm.clientHeight < 300){
+        elm.classList.remove('background-size-w', 'background-size-h');
+        return;
+      }
+
+      if(!elm.classList.contains('background-size-w') && !elm.classList.contains('background-size-h')){
+        return;
+      }
+
+      const imgWidth = elm.getAttribute('img-width');
+      const imgHeight = elm.getAttribute('img-height');
+      if(!imgWidth || !imgHeight || imgWidth === 0 || imgHeight === 0){
+        return;
+      }
+      if(elm.clientWidth / imgWidth >= elm.clientHeight / imgHeight){
+        elm.classList.add('background-size-w');
+        elm.classList.remove('background-size-h');
+      }else{
+        elm.classList.add('background-size-h');
+        elm.classList.remove('background-size-w');
+      }
+    });
+  }
+  onResize();
+  window.addEventListener('resize', onResize, {passive: true});
 
 });
